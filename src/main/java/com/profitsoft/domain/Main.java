2c260a600f9bd655f55fc6bd400e58ef5a3386a8
Oleg Sinelnikov <57261440+worksinelnikov@users.noreply.github.com>
2019-11-19 19:10:51 +0200

    Update Main.java

--- src/main/java/com/profitsoft/domain/Main.java
+++ src/main/java/com/profitsoft/domain/Main.java
@@ -11,11 +11,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-//        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-//        ctx.load("classpath:spring-config.xml"); //move from src.main.java to src.main.resources
-//        ctx.refresh();
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
-        System.out.println(applicationContext);
     }
 }
